869fb4605e76f42050c25aea7327c46c81eec5a1
Ansel <Ansel@Ansels-MacBook-Pro.local>
2016-06-06 17:32:47 +0800

    sd

--- Notification/AppDelegate.m
+++ Notification/AppDelegate.m
@@ -21,7 +21,7 @@
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     
     [self addObserver:self block:^(Notification * _Nonnull notification) {
-        NSLog(@"-------123-------");
+        NSLog(@"--------------");
     } name:@"Test" object:nil];
     
     [self addObserver:self selector:@selector(updateName:telphone:) name:@"PersonInfo" object:nil];
